8f239a9e0bcd7b81bcda752f861f22b37b29f604
csar56 <christoph.sauer@mni.thm.de>
2014-11-06 20:20:39 +0100

    partner changes

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Testseite</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php
+    	include('php/navbar.php');
+    ?>
+    
+    
+    	<div class="row-fluid">
+    			<div class="col-md-2"></div>
+    		<div class="col-md-4">
+    			<h3>Unnötige Überchrift:</h3>
+    			<p>Hier wird noch irgendetwas Sinvolles hin kommen. Ich weis nur noch nicht was.</p>
+    		</div>
+    			
+    			<div class="col-md-4">
+    				<h3>Impressum:</h3>
+    				<adress>
+	    				Julian-Pascal Martin<br />
+	    				Wilhelm-Bechtold-Str.6<br />
+	    				61206 Wöllstadt<br />
+	    				<a href="mailto:julian-pascal.martin@mni.thm.de">Email: julian-pascal.martin@mni.thm.de</a><br />
+	    				<a href="tel:+496034905350">Telefon: 06034905350</a><br />
+    				</adress>
+    			</div>
+    			<div class="col-md-2"></div>
+    			<div class="col-sm-12">
+    				<video>
+    					<source src="gource.webm"></srouce>
+    				</video>
+    			</div>
+    	</div>
+    
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ php/navbar.php
@@ -0,0 +1,12 @@
+<?php
+	echo '<nav class="navbar navbar-inverse">';
+	echo '<div class="navbar-header"><a href="index.php" class="navbar-brand">Index</a></div>';
+		echo '<div>';
+			echo '<ul class="nav navbar-nav">';
+				echo '<li><a href="http://christophsauer.cloudcontrolled.com">Seite meines Partners</a></<li>';
+				echo '<li><a href="http://www.burschenschaft.fellingshausen.de">Andere Beispielseite des Partners</a></<li>';
+				echo '<li><a href="#">Impressum</a></li>';
+			echo '</ul>';
+		echo '</div>';
+	echo '</nav>';
+?>
