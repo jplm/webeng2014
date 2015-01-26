920ad214e1561fb1ee8d61987ba2583010feb722
Julian-Pascal Martin <julian-pascal.martin@mni.thm.de>
2015-01-26 01:24:13 +0100

    shake

--- js/webformular.js
+++ js/webformular.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
+	/*
+	$('li').mouseover(function() {
+		$(this).highlight();
+	});
+	*/
 	$('#myForm').validate({
 		errorPlacement: function(error, element) {
 			$(element).closest( "form" ).append(error);
-			//"<div class='alert alert-danger' role='alert'>"+error+"</div>"
 		},
     rules: {
         search: {
@@ -15,6 +19,9 @@ $(document).ready(function() {
 					required: '<div class="alert alert-danger" role="alert">Das Suchfeld darf nicht leer sein!</div>',
 					maxlength: '<div class="alert alert-danger" role="alert">Der Suchtext darf Maximal 50 Zeichen lang sein!</div>'
 				}
-			}
+			},
+	invalidHandler: function(event, validator) {
+		$(this).effect('shake');
+	}
     });
 });
